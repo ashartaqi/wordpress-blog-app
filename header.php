cb95809601b489528ed85af802ee0d0ccc83e013
ashartaqi <95185892+ashartaqi@users.noreply.github.com>
2025-02-08 20:43:21 +0500

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
-    <meta name="author" content="https://youtube.com/FollowAndrew">
     <link rel="shortcut icon" href="/theme-template/images/logo.png" type="image/png">
 
 
